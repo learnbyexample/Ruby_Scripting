eced0f35a72037a8caf8278c6a8b9d16c0c83228
learnbyexample <learnbyexample@users.noreply.github.com>
2018-03-23 13:32:54 +0530

    two more exercises

---
+++ exercise_files/q3a_6by7.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+def six_by_seven(num)
+  # code your solution here
+end
+
+raise unless six_by_seven(66) == 'Food'
+raise unless six_by_seven(13) == 'Oops'
+raise unless six_by_seven(42) == 'Universe'
+raise unless six_by_seven(14) == 'Good'
+raise unless six_by_seven(84) == 'Universe'
+raise unless six_by_seven(235432) == 'Oops'
+
+puts 'all tests passed'
+
